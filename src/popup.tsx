e2dd0f9350341499b039741bffb38a67b351aeba
bhataasim1 <bhataasim700@gmail.com>
2024-07-08 21:29:07 +0530

    add icon in popup dialog box

---
+++ src/popup.tsx
@@ -0,0 +1,13 @@
+import { Icons } from "~common/Icons"
+
+import "~style.css"
+
+function IndexPopup() {
+  return (
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+      <Icons.AIIcon />
+    </div>
+  )
+}
+
+export default IndexPopup
